cf8c9caf6c94d94440cec1307d28bc631abb3ddd
PJ <bluedestiny88@gmail.com>
2022-11-29 03:15:17 +0000

    TMUX crashed tonight. Uploading to GitHub now before something worse happens

--- chadtweaks.py
+++ chadtweaks.py
@@ -10,13 +10,12 @@ pokemon = "https://pokeapi.co/api/v2/pokemon/"
 
 # Would this also technically fulfill the requirement for mini-project 1 if I decide to
 # make a new project for mini-project 2?
-@app.route("/starterpoke/<character>")
-def choose(character):
-    print(f"Hello {character}! \n")
+@app.route("/starterpoke", methods= ["POST"])
+def choose():
     # input won't work in flask, unfortunately.
+    # grabs value from postmaker.html
+    color= request.form.get("nm")
 
-    color = input("Pick a color: red, green, blue, yellow \n")
-    starter = ""
     if color == "red":
         starter = "charmander"
     elif color == "green":
@@ -25,16 +24,20 @@ def choose(character):
         starter == "squirtle"
     elif color == "yellow":
         starter == "pikachu"
+    else:
+        starter= "muk"
+    
     ## Return statement needed for else?
     getthatpokemon  = requests.get(pokemon + starter)
     pokemondata = getthatpokemon.json()
     attack = pokemondata["abilities"][0]["ability"]["name"]
-    return f"{starter.capitalize()} used {attack.capitalize()}! It's super effective!"
+    return f"Hello! Your starting Pokemon is {starter}! \n {starter.capitalize()} used {attack.capitalize()}! It's super effective!"
 
 @app.route("/start/")
 def start():
-    player = input("Please enter your name: ")
-    return redirect(url_for("choose", character = player))
+    # take postmaker.html from an earlier lab. Have it ask to choose one of the following colors: red, green, blue, or yellow.
+    # change the POST address to /starterpoke
+    return render_template("choice.html")
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 2224)
